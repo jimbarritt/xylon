82e1634550a0072e1717f65566ab0db0959dc4c4
Jim Barritt <jim@planet-ix.com>
2010-09-05 21:14:33 +0100

    Some minor checkin

--- tool/checkstyle/checkstyle2tm3.rb
+++ tool/checkstyle/checkstyle2tm3.rb
@@ -3,52 +3,53 @@ require 'nokogiri'
 
 # Provided by Erik Doernenburg (http://erik.doernenburg.com/2010/05/metrics-tree-maps/)
 class Metrics
-  
-  PATH_SEP = /\//                  
-    
+
+  PATH_SEP = /\//
+
   def initialize(name)
-      @name = name
-      @flength = 0
-      @cdac = 0
-      @cfoc = 0
-      @mcount = 0
-      @wmc_len = 0
-      @wmc_cc = 0
+    @name = name
+    @flength = 0
+    @cdac = 0
+    @cfoc = 0
+    @mcount = 0
+    @wmc_len = 0
+    @wmc_cc = 0
   end
 
   def add(source, message)
     case source.split(/\./).last
-      when "FileLengthCheck" 
+      when "FileLengthCheck"
         @flength = getval(message, 3)
-      when "ClassDataAbstractionCouplingCheck" 
+      when "ClassDataAbstractionCouplingCheck"
         @cdac = getval(message, 5)
-      when "ClassFanOutComplexityCheck" 
+      when "ClassFanOutComplexityCheck"
         @cfoc = getval(message, 4)
-      when "MethodLengthCheck" 
-     	  @wmc_len += getval(message, 3) 
-      when "CyclomaticComplexityCheck" 
+      when "MethodLengthCheck"
+        @wmc_len += getval(message, 3)
+      when "CyclomaticComplexityCheck"
         @wmc_cc += getval(message, 3)
         @mcount += 1
     end
   end
-     
+
   def getval(message, pos)
-	  message.split(/ /)[pos].sub(/,/, "").to_i
+    message.split(/ /)[pos].sub(/,/, "").to_i
   end
 
   def self.titles
-    "FLENGTH\tCDAC\tCFOC\tMCOUNT\tWMCloc\tWMCcc"       
-  end       
-  
+    "FLENGTH\tCDAC\tCFOC\tMCOUNT\tWMCloc\tWMCcc\tAVGMCcc"
+  end
+
   def self.types
-    "INTEGER\tINTEGER\tINTEGER\tINTEGER\tINTEGER\tINTEGER"
+    "INTEGER\tINTEGER\tINTEGER\tINTEGER\tINTEGER\tINTEGER\tINTEGER"
   end
-  
-  def to_s    
-    path = @name.chomp(".java").split(PATH_SEP).join("\t") 
-    return "#{@flength}\t#{@cdac}\t#{@cfoc}\t#{@mcount}\t#{@wmc_len}\t#{@wmc_cc}\t#{path}"
+
+  def to_s
+    path = @name.chomp(".java").split(PATH_SEP).join("\t")
+    avgmcc = (@mcount > 0) ? (@wmc_cc / @mcount) : 0
+    return "#{@flength}\t#{@cdac}\t#{@cfoc}\t#{@mcount}\t#{@wmc_len}\t#{@wmc_cc}\t#{avgmcc}\t#{path}"
   end
-  
+
 end
 
 
@@ -56,21 +57,21 @@ class Processor
 
   def initialize(filename, prefix)
     @doc = Nokogiri::XML(File.new(filename))
-    @prefix = prefix    
+    @prefix = prefix
   end
 
   def run
     puts Metrics.titles
     puts Metrics.types
-    @doc.xpath("//file").each do |filenode|    
+    @doc.xpath("//file").each do |filenode|
       metrics = Metrics.new(filenode["name"].sub(@prefix, ""))
-      filenode.xpath(".//error").each do |errnode| 
+      filenode.xpath(".//error").each do |errnode|
         metrics.add(errnode["source"], errnode["message"])
-      end  
+      end
       puts metrics.to_s
     end
   end
-  
+
 end
 
 Processor.new(ARGV[0], ARGV[1]).run
