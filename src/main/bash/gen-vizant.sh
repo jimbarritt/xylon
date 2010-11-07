d72d4b837a06c6790422409e829cbab4a3db608a
Jim Barritt <jim@planet-ix.com>
2010-11-07 18:11:16 +0000

    Adding vizant and making it work

--- src/main/bash/gen-vizant.sh
+++ src/main/bash/gen-vizant.sh
@@ -3,20 +3,18 @@
 PROJECT_NAME=$1
 VIZANT_HOME=$2
 
-TARGET_DIR=$PWD/target/vizant
+TARGET_DIR=$PWD/target
 
 BUILD_XML=$PWD/build.xml
 
-TARGET_FILE=$TARGET_DIR/$PROJECT_NAME-build.png
-
-echo "Generating vizant (png) file to $TARGET_FILE"
-
-VIZANT_ANT_SCRIPT="$VIZANT_HOME/ant/checkstyle.xml"
+VIZANT_ANT_SCRIPT="$VIZANT_HOME/ant/vizant.xml"
 
 mkdir -p $TARGET_DIR
-
+             
 ANT_CMD="ant -f $VIZANT_ANT_SCRIPT"
 ANT_PROPERTIES="$ANT_PROPERTIES -Dbasedir=$VIZANT_HOME/ant/"
+ANT_PROPERTIES="$ANT_PROPERTIES -Dproject.name=$PROJECT_NAME"
+ANT_PROPERTIES="$ANT_PROPERTIES -Dtarget.dir=$TARGET_DIR"
 ANT_PROPERTIES="$ANT_PROPERTIES -Dbuild.xml=$BUILD_XML"
 
 echo "Executing: ant to generate vizualisation of the ant file ..."
